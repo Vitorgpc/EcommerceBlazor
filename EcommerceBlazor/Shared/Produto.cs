79d08500912c36bedbc9738cd518418bb2841f06
Vitorgpc <vitorcolazelli@gmail.com>
2023-09-21 09:42:40 -0300

    Desenvolvendo primeira pagina do sistema com chamada para API

---
+++ EcommerceBlazor/Shared/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceBlazor.Shared
+{
+    public class Produto
+    {
+        public int Produto_ID { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty;
+        public string UrlImagem { get; set; } = string.Empty;
+        public decimal Preco { get; set; }
+    }
+}

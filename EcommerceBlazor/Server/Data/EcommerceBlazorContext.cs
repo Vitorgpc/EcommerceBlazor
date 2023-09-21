963e2435e90d4902f2ac199e2d5b8be10a555129
Vitorgpc <vitorcolazelli@gmail.com>
2023-09-21 20:29:05 -0300

    Adicionando conexao com banco de dados

---
+++ EcommerceBlazor/Server/Data/EcommerceBlazorContext.cs
@@ -0,0 +1,12 @@
+﻿namespace EcommerceBlazor.Server.Data
+{
+    public class EcommerceBlazorContext : DbContext
+    {
+        public EcommerceBlazorContext(DbContextOptions<EcommerceBlazorContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
--- EcommerceBlazor/Server/Program.cs
+++ EcommerceBlazor/Server/Program.cs
@@ -1,9 +1,15 @@
 global using EcommerceBlazor.Shared;
+global using Microsoft.EntityFrameworkCore;
+using EcommerceBlazor.Server.Data;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<EcommerceBlazorContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();

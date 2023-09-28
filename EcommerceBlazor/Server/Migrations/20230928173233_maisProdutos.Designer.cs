407c44e184ac1af9e8a483b738bb3278de6f95c1
Vitorgpc <vitorcolazelli@gmail.com>
2023-09-28 15:08:41 -0300

    adicionando mais produtos por seed

--- EcommerceBlazor/Server/Migrations/20230928173233_maisProdutos.Designer.cs
+++ EcommerceBlazor/Server/Migrations/20230928173233_maisProdutos.Designer.cs
@@ -3,6 +3,7 @@ using EcommerceBlazor.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EcommerceBlazor.Server.Migrations
 {
     [DbContext(typeof(EcommerceBlazorContext))]
-    partial class EcommerceBlazorContextModelSnapshot : ModelSnapshot
+    [Migration("20230928173233_maisProdutos")]
+    partial class maisProdutos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

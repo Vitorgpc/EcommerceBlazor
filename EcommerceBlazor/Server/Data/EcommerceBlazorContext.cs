cf6450740db410b9fd78ee02fb4e1d65cd5af7e4
Vitorgpc <vitorcolazelli@gmail.com>
2023-09-29 12:05:41 -0300

    adcionando migrations

--- EcommerceBlazor/Server/Data/EcommerceBlazorContext.cs
+++ EcommerceBlazor/Server/Data/EcommerceBlazorContext.cs
@@ -9,6 +9,23 @@
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<TipoProduto>().HasKey(x => x.TipoProduto_ID);
+
+            modelBuilder.Entity<TipoProduto>().HasData(
+                new TipoProduto { TipoProduto_ID = 1, Nome = "Default" },
+                new TipoProduto { TipoProduto_ID = 2, Nome = "Paperback" },
+                new TipoProduto { TipoProduto_ID = 3, Nome = "E-Book" },
+                new TipoProduto { TipoProduto_ID = 4, Nome = "Audiobook" },
+                new TipoProduto { TipoProduto_ID = 5, Nome = "Stream" },
+                new TipoProduto { TipoProduto_ID = 6, Nome = "Blu-ray" },
+                new TipoProduto { TipoProduto_ID = 7, Nome = "VHS" },
+                new TipoProduto { TipoProduto_ID = 8, Nome = "PC" },
+                new TipoProduto { TipoProduto_ID = 9, Nome = "PlayStation" },
+                new TipoProduto { TipoProduto_ID = 10, Nome = "Xbox" }
+           );
+
+            modelBuilder.Entity<ProdutoVariante>().HasKey(x => new { x.ProdutoId, x.TipoProdutoId });
+
             modelBuilder.Entity<Categoria>().HasKey(x => x.Categoria_ID);
             modelBuilder.Entity<Categoria>().HasData(
                 new Categoria
@@ -38,7 +55,6 @@
                     Produto_ID = 1,
                     Nome = "The Hitchhiker's Guide to the Galaxy",
                     Descricao = "The Hitchhiker's Guide to the Galaxy[a][b] is a comedy science fiction franchise created by Douglas Adams. Originally a 1978 radio comedy broadcast on BBC Radio 4, it was later adapted to other formats, including novels, stage shows, comic books, a 1981 TV series, a 1984 text adventure game, and 2005 feature film.",
-                    Preco = 9.99m,
                     UrlImagem = "https://upload.wikimedia.org/wikipedia/en/b/bd/H2G2_UK_front_cover.jpg",
                     CategoriaId = 1
                 },
@@ -47,7 +63,6 @@
                     Produto_ID = 2,
                     Nome = "Ready Player One",
                     Descricao = "Ready Player One is a 2011 science fiction novel, and the debut novel of American author Ernest Cline. The story, set in a dystopia in 2045, follows protagonist Wade Watts on his search for an Easter egg in a worldwide virtual reality game, the discovery of which would lead him to inherit the game creator's fortune. Cline sold the rights to publish the novel in June 2010, in a bidding war to the Crown Publishing Group (a division of Random House).[1] The book was published on August 16, 2011.[2] An audiobook was released the same day; it was narrated by Wil Wheaton, who was mentioned briefly in one of the chapters.[3][4]Ch. 20 In 2012, the book received an Alex Award from the Young Adult Library Services Association division of the American Library Association[5] and won the 2011 Prometheus Award.[6]",
-                    Preco = 7.99m,
                     UrlImagem = "https://upload.wikimedia.org/wikipedia/en/a/a4/Ready_Player_One_cover.jpg",
                     CategoriaId = 1
                 },
@@ -56,7 +71,6 @@
                     Produto_ID = 3,
                     Nome = "Nineteen Eighty-Four",
                     Descricao = "Mil Novecentos e Oitenta e Quatro (em inglês: Nineteen Eighty-Four), muitas vezes publicado como 1984, é um romance distópico da autoria do escritor britânico George Orwell e publicado em 1949.[1][2] O romance é ambientado na Pista de Pouso Número 1 (anteriormente conhecida como Grã-Bretanha), uma província do superestado da Oceania, em um mundo de guerra perpétua, vigilância governamental onipresente e manipulação pública e histórica. Os habitantes deste superestado são ditados por um",
-                    Preco = 8.99m,
                     UrlImagem = "https://upload.wikimedia.org/wikipedia/pt/7/74/1984cover.jpg",
                     CategoriaId = 1
                 },
@@ -67,7 +81,6 @@
                     Descricao = "The Matrix is a 1999 science fiction action film written and directed by the Wachowskis, and produced by Joel Silver. Starring Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss, Hugo Weaving, and Joe Pantoliano, and as the first installment in the Matrix franchise, it depicts a dystopian future in which humanity is unknowingly trapped inside a simulated reality, the Matrix, which intelligent machines have created to distract humans while using their bodies as an energy source. When computer programmer Thomas Anderson, under the hacker alias \"Neo\", uncovers the truth, he \"is drawn into a rebellion against the machines\" along with other people who have been freed from the Matrix.",
                     UrlImagem = "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg",
                     CategoriaId = 2,
-                    Preco = 15.98m
                 },
                 new Produto
                 {
@@ -76,7 +89,6 @@
                     Nome = "Back to the Future",
                     Descricao = "Back to the Future is a 1985 American science fiction film directed by Robert Zemeckis. Written by Zemeckis and Bob Gale, it stars Michael J. Fox, Christopher Lloyd, Lea Thompson, Crispin Glover, and Thomas F. Wilson. Set in 1985, the story follows Marty McFly (Fox), a teenager accidentally sent back to 1955 in a time-traveling DeLorean automobile built by his eccentric scientist friend Doctor Emmett \"Doc\" Brown (Lloyd). Trapped in the past, Marty inadvertently prevents his future parents' meeting—threatening his very existence—and is forced to reconcile the pair and somehow get back to the future.",
                     UrlImagem = "https://upload.wikimedia.org/wikipedia/en/d/d2/Back_to_the_Future.jpg",
-                    Preco = 16m
                 },
                 new Produto
                 {
@@ -85,7 +97,6 @@
                     Nome = "Toy Story",
                     Descricao = "Toy Story is a 1995 American computer-animated comedy film produced by Pixar Animation Studios and released by Walt Disney Pictures. The first installment in the Toy Story franchise, it was the first entirely computer-animated feature film, as well as the first feature film from Pixar. The film was directed by John Lasseter (in his feature directorial debut), and written by Joss Whedon, Andrew Stanton, Joel Cohen, and Alec Sokolow from a story by Lasseter, Stanton, Pete Docter, and Joe Ranft. The film features music by Randy Newman, was produced by Bonnie Arnold and Ralph Guggenheim, and was executive-produced by Steve Jobs and Edwin Catmull. The film features the voices of Tom Hanks, Tim Allen, Don Rickles, Wallace Shawn, John Ratzenberger, Jim Varney, Annie Potts, R. Lee Ermey, John Morris, Laurie Metcalf, and Erik von Detten. Taking place in a world where anthropomorphic toys come to life when humans are not present, the plot focuses on the relationship between an old-fashioned pull-string cowboy doll named Woody and an astronaut action figure, Buzz Lightyear, as they evolve from rivals competing for the affections of their owner, Andy Davis, to friends who work together to be reunited with Andy after being separated from him.",
                     UrlImagem = "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                    Preco = 15.98m
                 },
                 new Produto
                 {
@@ -94,7 +105,6 @@
                     Nome = "Half-Life 2",
                     Descricao = "Half-Life 2 is a 2004 first-person shooter game developed and published by Valve. Like the original Half-Life, it combines shooting, puzzles, and storytelling, and adds features such as vehicles and physics-based gameplay.",
                     UrlImagem = "https://upload.wikimedia.org/wikipedia/en/2/25/Half-Life_2_cover.jpg",
-                    Preco = 215.99m
                 },
                 new Produto
                 {
@@ -103,7 +113,6 @@
                     Nome = "Diablo II",
                     Descricao = "Diablo II is an action role-playing hack-and-slash computer video game developed by Blizzard North and published by Blizzard Entertainment in 2000 for Microsoft Windows, Classic Mac OS, and macOS.",
                     UrlImagem = "https://upload.wikimedia.org/wikipedia/en/d/d5/Diablo_II_Coverart.png",
-                    Preco = 299.99m
                 },
                 new Produto
                 {
@@ -112,7 +121,6 @@
                     Nome = "Day of the Tentacle",
                     Descricao = "Day of the Tentacle, also known as Maniac Mansion II: Day of the Tentacle, is a 1993 graphic adventure game developed and published by LucasArts. It is the sequel to the 1987 game Maniac Mansion.",
                     UrlImagem = "https://upload.wikimedia.org/wikipedia/en/7/79/Day_of_the_Tentacle_artwork.jpg",
-                    Preco = 299.99m
                 },
                 new Produto
                 {
@@ -121,7 +129,6 @@
                     Nome = "Xbox",
                     Descricao = "The Xbox is a home video game console and the first installment in the Xbox series of video game consoles manufactured by Microsoft.",
                     UrlImagem = "https://upload.wikimedia.org/wikipedia/commons/4/43/Xbox-console.jpg",
-                    Preco = 1200m
                 },
                 new Produto
                 {
@@ -130,12 +137,126 @@
                     Nome = "Super Nintendo Entertainment System",
                     Descricao = "The Super Nintendo Entertainment System (SNES), also known as the Super NES or Super Nintendo, is a 16-bit home video game console developed by Nintendo that was released in 1990 in Japan and South Korea.",
                     UrlImagem = "https://upload.wikimedia.org/wikipedia/commons/e/ee/Nintendo-Super-Famicom-Set-FL.jpg",
-                    Preco = 2200.65m
+                }
+            );
+
+            modelBuilder.Entity<ProdutoVariante>().HasData(
+                new ProdutoVariante
+                {
+                    ProdutoId = 1,
+                    TipoProdutoId = 2,
+                    Preco = 9.99m,
+                    PrecoOriginal = 19.99m
+                },
+                new ProdutoVariante
+                {
+                    ProdutoId = 1,
+                    TipoProdutoId = 3,
+                    Preco = 7.99m
+                },
+                new ProdutoVariante
+                {
+                    ProdutoId = 1,
+                    TipoProdutoId = 4,
+                    Preco = 19.99m,
+                    PrecoOriginal = 29.99m
+                },
+                new ProdutoVariante
+                {
+                    ProdutoId = 2,
+                    TipoProdutoId = 2,
+                    Preco = 7.99m,
+                    PrecoOriginal = 14.99m
+                },
+                new ProdutoVariante
+                {
+                    ProdutoId = 3,
+                    TipoProdutoId = 2,
+                    Preco = 6.99m
+                },
+                new ProdutoVariante
+                {
+                    ProdutoId = 4,
+                    TipoProdutoId = 5,
+                    Preco = 3.99m
+                },
+                new ProdutoVariante
+                {
+                    ProdutoId = 4,
+                    TipoProdutoId = 6,
+                    Preco = 9.99m
+                },
+                new ProdutoVariante
+                {
+                    ProdutoId = 4,
+                    TipoProdutoId = 7,
+                    Preco = 19.99m
+                },
+                new ProdutoVariante
+                {
+                    ProdutoId = 5,
+                    TipoProdutoId = 5,
+                    Preco = 3.99m,
+                },
+                new ProdutoVariante
+                {
+                    ProdutoId = 6,
+                    TipoProdutoId = 5,
+                    Preco = 2.99m
+                },
+                new ProdutoVariante
+                {
+                    ProdutoId = 7,
+                    TipoProdutoId = 8,
+                    Preco = 19.99m,
+                    PrecoOriginal = 29.99m
+                },
+                new ProdutoVariante
+                {
+                    ProdutoId = 7,
+                    TipoProdutoId = 9,
+                    Preco = 69.99m
+                },
+                new ProdutoVariante
+                {
+                    ProdutoId = 7,
+                    TipoProdutoId = 10,
+                    Preco = 49.99m,
+                    PrecoOriginal = 59.99m
+                },
+                new ProdutoVariante
+                {
+                    ProdutoId = 8,
+                    TipoProdutoId = 8,
+                    Preco = 9.99m,
+                    PrecoOriginal = 24.99m,
+                },
+                new ProdutoVariante
+                {
+                    ProdutoId = 9,
+                    TipoProdutoId = 8,
+                    Preco = 14.99m
+                },
+                new ProdutoVariante
+                {
+                    ProdutoId = 10,
+                    TipoProdutoId = 1,
+                    Preco = 159.99m,
+                    PrecoOriginal = 299m
+                },
+                new ProdutoVariante
+                {
+                    ProdutoId = 11,
+                    TipoProdutoId = 1,
+                    Preco = 79.99m,
+                    PrecoOriginal = 399m
                 }
             );
         }
 
         public DbSet<Produto> Produto { get; set; }
         public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<TipoProduto> TipoProduto { get; set; }
+        public DbSet<ProdutoVariante> ProdutoVariante { get; set; }
     }
 }
---
+++ EcommerceBlazor/Server/Migrations/20230929144906_Variante.cs
@@ -0,0 +1,199 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EcommerceBlazor.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Variante : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Preco",
+                table: "Produto");
+
+            migrationBuilder.CreateTable(
+                name: "TipoProduto",
+                columns: table => new
+                {
+                    TipoProduto_ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoProduto", x => x.TipoProduto_ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProdutoVariante",
+                columns: table => new
+                {
+                    ProdutoId = table.Column<int>(type: "int", nullable: false),
+                    TipoProdutoId = table.Column<int>(type: "int", nullable: false),
+                    Preco = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PrecoOriginal = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProdutoVariante", x => new { x.ProdutoId, x.TipoProdutoId });
+                    table.ForeignKey(
+                        name: "FK_ProdutoVariante_Produto_ProdutoId",
+                        column: x => x.ProdutoId,
+                        principalTable: "Produto",
+                        principalColumn: "Produto_ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProdutoVariante_TipoProduto_TipoProdutoId",
+                        column: x => x.TipoProdutoId,
+                        principalTable: "TipoProduto",
+                        principalColumn: "TipoProduto_ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TipoProduto",
+                columns: new[] { "TipoProduto_ID", "Nome" },
+                values: new object[,]
+                {
+                    { 1, "Default" },
+                    { 2, "Paperback" },
+                    { 3, "E-Book" },
+                    { 4, "Audiobook" },
+                    { 5, "Stream" },
+                    { 6, "Blu-ray" },
+                    { 7, "VHS" },
+                    { 8, "PC" },
+                    { 9, "PlayStation" },
+                    { 10, "Xbox" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProdutoVariante",
+                columns: new[] { "ProdutoId", "TipoProdutoId", "Preco", "PrecoOriginal" },
+                values: new object[,]
+                {
+                    { 1, 2, 9.99m, 19.99m },
+                    { 1, 3, 7.99m, 0m },
+                    { 1, 4, 19.99m, 29.99m },
+                    { 2, 2, 7.99m, 14.99m },
+                    { 3, 2, 6.99m, 0m },
+                    { 4, 5, 3.99m, 0m },
+                    { 4, 6, 9.99m, 0m },
+                    { 4, 7, 19.99m, 0m },
+                    { 5, 5, 3.99m, 0m },
+                    { 6, 5, 2.99m, 0m },
+                    { 7, 8, 19.99m, 29.99m },
+                    { 7, 9, 69.99m, 0m },
+                    { 7, 10, 49.99m, 59.99m },
+                    { 8, 8, 9.99m, 24.99m },
+                    { 9, 8, 14.99m, 0m },
+                    { 10, 1, 159.99m, 299m },
+                    { 11, 1, 79.99m, 399m }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProdutoVariante_TipoProdutoId",
+                table: "ProdutoVariante",
+                column: "TipoProdutoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProdutoVariante");
+
+            migrationBuilder.DropTable(
+                name: "TipoProduto");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Preco",
+                table: "Produto",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.UpdateData(
+                table: "Produto",
+                keyColumn: "Produto_ID",
+                keyValue: 1,
+                column: "Preco",
+                value: 9.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Produto",
+                keyColumn: "Produto_ID",
+                keyValue: 2,
+                column: "Preco",
+                value: 7.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Produto",
+                keyColumn: "Produto_ID",
+                keyValue: 3,
+                column: "Preco",
+                value: 8.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Produto",
+                keyColumn: "Produto_ID",
+                keyValue: 4,
+                column: "Preco",
+                value: 15.98m);
+
+            migrationBuilder.UpdateData(
+                table: "Produto",
+                keyColumn: "Produto_ID",
+                keyValue: 5,
+                column: "Preco",
+                value: 16m);
+
+            migrationBuilder.UpdateData(
+                table: "Produto",
+                keyColumn: "Produto_ID",
+                keyValue: 6,
+                column: "Preco",
+                value: 15.98m);
+
+            migrationBuilder.UpdateData(
+                table: "Produto",
+                keyColumn: "Produto_ID",
+                keyValue: 7,
+                column: "Preco",
+                value: 215.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Produto",
+                keyColumn: "Produto_ID",
+                keyValue: 8,
+                column: "Preco",
+                value: 299.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Produto",
+                keyColumn: "Produto_ID",
+                keyValue: 9,
+                column: "Preco",
+                value: 299.99m);
+
+            migrationBuilder.UpdateData(
+                table: "Produto",
+                keyColumn: "Produto_ID",
+                keyValue: 10,
+                column: "Preco",
+                value: 1200m);
+
+            migrationBuilder.UpdateData(
+                table: "Produto",
+                keyColumn: "Produto_ID",
+                keyValue: 11,
+                column: "Preco",
+                value: 2200.65m);
+        }
+    }
+}

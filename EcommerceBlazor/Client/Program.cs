706def80f2ef7f38c1600f12929dac57ddefaa39
Vitorgpc <vitorcolazelli@gmail.com>
2023-10-21 18:46:32 -0300

    finalizando desenvolvimento de multiplas imagens nos produtos

--- EcommerceBlazor/Client/Program.cs
+++ EcommerceBlazor/Client/Program.cs
@@ -13,12 +13,14 @@ using Blazored.LocalStorage;
 using EcommerceBlazor.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddMudServices();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
